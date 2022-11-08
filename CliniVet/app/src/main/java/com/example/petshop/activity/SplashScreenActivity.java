5bcb02b345c989df08e330e8b60a675e29515d80
FelipeRaulino <felipe.raaulino29@gmail.com>
2022-11-08 08:51:32 -0300

    escondendo a status bar da splash screen

--- CliniVet/app/src/main/java/com/example/petshop/activity/SplashScreenActivity.java
+++ CliniVet/app/src/main/java/com/example/petshop/activity/SplashScreenActivity.java
@@ -1,8 +1,12 @@
 package com.example.petshop.activity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +22,20 @@ public class SplashScreenActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
         new Handler().postDelayed(new Runnable() {
             @Override
