07945c10ce3079c40efcae5c33af8bfc54af228b
FelipeRaulino <felipe.raaulino29@gmail.com>
2022-11-07 21:23:26 -0300

    adicionando splash screen

---
+++ CliniVet/app/src/main/java/com/example/petshop/activity/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.petshop.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.petshop.activity.autenticacao.LoginActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.petshop.R.layout.activity_splash_screen);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
+
+    }
+}
